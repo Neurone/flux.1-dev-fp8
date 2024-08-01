e2944a68e8d45da40789928aa451bd9f9069bd6a
Apolinário from multimodal AI art <multimodalart@users.noreply.huggingface.co>
2024-08-01 11:56:28 +0000

    Update app.py

--- app.py
+++ app.py
@@ -57,17 +57,16 @@ examples = [
 css="""
 #col-container {
     margin: 0 auto;
-    max-width: 550px;
+    max-width: 520px;
 }
 """
 
 with gr.Blocks(css=css) as demo:
     
     with gr.Column(elem_id="col-container"):
-        gr.Markdown(f"""
-        # FLUX.1 Schnell
-        12B param rectified flow transformer distilled from [FLUX.1 Pro](https://blackforestlabs.ai/) for 4 step generation
-        [[blog](https://blackforestlabs.ai/2024/07/31/announcing-black-forest-labs/) [model](https://black-forest-labs/FLUX.1-schnell)]
+        gr.Markdown(f"""# FLUX.1 Schnell
+12B param rectified flow transformer distilled from [FLUX.1 Pro](https://blackforestlabs.ai/) for 4 step generation
+[[[blog](https://blackforestlabs.ai/2024/07/31/announcing-black-forest-labs/)] [[model](https://black-forest-labs/FLUX.1-schnell)]]
         """)
         
         with gr.Row():
